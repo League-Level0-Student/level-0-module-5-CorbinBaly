2ad59947a8755ab77be80b78263e826da39cfcda
league <league@wts9.attlocal.net>
2018-09-27 19:59:01 -0700

    Finished EvilPolygons and CircleRing.

--- src/extra/EvilPolygonsChallenge.java
+++ src/extra/EvilPolygonsChallenge.java
@@ -1,4 +1,5 @@
 package extra;
+
 import java.awt.Color;
 
 import javax.swing.JOptionPane;
@@ -6,24 +7,40 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class EvilPolygonsChallenge {
-	
+
 	public static void main(String[] args) {
-		
-		//1. Create a new Robot
 
-		//2. Set the speed to 100
+		// 1. Create a new Robot
+		Robot r2d3 = new Robot();
+		// 2. Set the speed to 100
+		r2d3.setSpeed(100);
+		int colorChoice = JOptionPane.showOptionDialog(null, "What Color?", "title", JOptionPane.INFORMATION_MESSAGE,
+				JOptionPane.DEFAULT_OPTION, null, new String[] { "Red", "Green", "Blue" }, 0);
+		r2d3.penDown();
+		// 3. Use if statements to check the the value of colorChoice and set the pen
+		// color accordingly
+		if (colorChoice == 0) {
+			r2d3.setPenColor(255, 0, 0);
+		} else if (colorChoice == 1) {
 
-		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Color 1","Color 2", "Color 3"}, 0);
-		
-		//3. Use if statements to check the the value of colorChoice and set the pen color accordingly 
-		
-		//4. Ask the use how many polygons they want to be drawn.
-		
-		//5. Use the robot to draw the number of polygons the user requested.
-		
-		//6. Make it so your shapes do not overlap
+			r2d3.setPenColor(0, 255, 0);
+		} else {
+			r2d3.setPenColor(0, 0, 255);
+		}
+		// 4. Ask the use how many polygons they want to be drawn.
+		String Quest = JOptionPane.showInputDialog("How many polygons do you want to be drawn?");
+		// 5. Use the robot to draw the number of polygons the user requested.
+		int Num = Integer.parseInt(Quest);
+		for (int i = 0; i < Num; i++) {
 
-		//7. Challenge: add more colors to the Option Dialog.
+			r2d3.turn(90);
+			r2d3.move(200);
+			r2d3.turn(90);
+			r2d3.move(200);
+			r2d3.move(200);
+		}
+		// 6. Make it so your shapes do not overlap
+
+		// 7. Challenge: add more colors to the Option Dialog.
 	}
 }
-
